3cb329ae4f8ae154a94ffcb92e6ee4cf63cbceb2
carles belmonte <140880473+crlxs@users.noreply.github.com>
2026-09-15 14:55:52 +0200

    docker.sh update

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,6 +1,9 @@
-# Add Docker's official GPG key:
+# Update system
 sudo apt update
-sudo apt install ca-certificates curl
+sudo apt upgrade -y
+
+# Add Docker's official GPG key:
+sudo apt install ca-certificates curl -y
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -15,4 +18,4 @@ Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
